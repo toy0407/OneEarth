347cfcc44e9b4a615165cb59fc79c29164185535
toy0407 <bose.suvro@gmail.com>
2022-07-09 22:02:00 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/oneearthmyplanet/one_earth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.oneearthmyplanet.one_earth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import './strings_manager.dart';
+
+class Routes {
+  static const String splashRoute = "/";
+  static const String onBoardingRoute = "/onBoarding";
+  static const String loginRoute = "/login";
+  static const String registerRoute = "/register";
+  static const String forgotPasswordRoute = "/forgotPassword";
+  static const String mainRoute = "/main";
+  static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      // case Routes.splashRoute:
+      //   return MaterialPageRoute(builder: (_) => SplashView());
+      // case Routes.loginRoute:
+      //   return MaterialPageRoute(builder: (_) => LoginView());
+      // case Routes.onBoardingRoute:
+      //   return MaterialPageRoute(builder: (_) => OnBoardingView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: Text(AppStrings.noRouteFound),
+              ),
+              body: Center(child: Text(AppStrings.noRouteFound)),
+            ));
+  }
+}
