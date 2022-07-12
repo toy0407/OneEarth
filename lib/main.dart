fcad9e5951bbba29d56e9f2a25b4314cfb81f71f
Ritwik Das <dasritwik73@gmail.com>
2022-07-12 15:25:46 +0530

    Initialized firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:one_earth/firebase_options.dart';
 import 'package:one_earth/presentation/login/auth_repository.dart';
 import 'package:one_earth/presentation/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
