975c2cc8b5d1659cc34d6f24f80c965d8d4c74a5
Ritwik Das <dasritwik73@gmail.com>
2022-07-12 13:15:02 +0530

    Made some changes in login_state.dart

--- lib/presentation/login/login_state.dart
+++ lib/presentation/login/login_state.dart
@@ -9,12 +9,4 @@ class LoginState {
       {this.email = '',
       this.password = '',
       this.formStatus = const InitialFormStatus()});
-
-  // LoginState copyWith(
-  //     {String? email, String? password, FormSubmissionStatus? formStatus}) {
-  //   return LoginState(
-  //       email: email ?? this.email,
-  //       password: password ?? this.password,
-  //       formStatus: formStatus ?? this.formStatus);
-  // }
 }
