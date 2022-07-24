338ef43b4735572ccd068064c1d2be72e0d2284e
Ritwik Das <dasritwik73@gmail.com>
2022-07-24 12:38:24 +0530

    Fixed build issues

--- lib/data/home/home_data.dart
+++ lib/data/home/home_data.dart
@@ -1,4 +1,45 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
 class HomeRepository {
+
+  late String question;
+  late int answerIndex;
+  late List<String> options;
+
+  getTrivia() async {
+
+    String _question = 'NULL';
+    int _answer = -1;
+    List<String> _options = [];
+    int randomTriviaId = Random().nextInt(2) + 1;
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference _collectionRef =
+    FirebaseFirestore.instance.collection('Trivia');
+    QuerySnapshot querySnapshot = await _collectionRef.where('id', isEqualTo: randomTriviaId).limit(1).get();
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+    print(allData);
+    // triviaReference
+    //     // .where('id', isEqualTo: randomTriviaId)
+    //     // .limit(1)
+    //     .then((snapshot) =>
+    // {
+    //   // print(snapshot.size);
+    //   if (snapshot.size > 0)
+    //     {
+    //       snapshot.docs.forEach((element) {
+    //         print(element);
+    //         _question = element['question'];
+    //         _answer = element['answer'];
+    //         _options = element['options'];
+    //       })
+    //     }
+    // });
+    // debugPrint("Question $_question ; AnswerIndex $_answer ; options $_options");
+  }
+
   Future<String> getData(String type) async {
     // Emulate network delay
     return await Future.delayed(const Duration(seconds: 2)).then((_) {
@@ -7,6 +48,8 @@ class HomeRepository {
           return "leaderboard is coming !";
 
         case "my_space":
+          print("hi");
+          getTrivia();
           return "My Space";
 
         case "my_activities":
@@ -15,4 +58,14 @@ class HomeRepository {
       }
     });
   }
+}
+
+class Trivia {
+
+
+  Trivia({required String question,
+    required int answerIndex,
+    required List<String> options});
+
+
 }
