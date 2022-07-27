127199f9852039c6212b7edd928d3c27db2abb1a
Ritwik Das <dasritwik73@gmail.com>
2022-07-27 23:41:27 +0530

    Added validations in login and register page

--- lib/presentation/login/login_screen.dart
+++ lib/presentation/login/login_screen.dart
@@ -5,9 +5,9 @@ import 'package:one_earth/presentation/resources/color_manager.dart';
 import 'package:one_earth/presentation/resources/font_manager.dart';
 import 'package:one_earth/presentation/resources/styles_manager.dart';
 import 'package:one_earth/presentation/resources/values_manager.dart';
-import 'package:one_earth/temp/if_debugging.dart';
 import 'package:one_earth/presentation/login/bloc/login_bloc.dart';
 import 'package:one_earth/presentation/login/bloc/login_event.dart';
+import 'package:email_validator/email_validator.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -19,6 +19,9 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
+
+  final formKey = GlobalKey<FormState>();
+  bool isValid = false;
 
   @override
   void initState() {
@@ -37,72 +40,106 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    _emailController.addListener(
+      () {
+        if (EmailValidator.validate(_emailController.text)) {
+          setState(() {
+            isValid = true;
+          });
+        } else {
+          setState(
+            () {
+              isValid = false;
+            },
+          );
+        }
+      },
+    );
+
     var mediaQuery = MediaQuery.of(context).size;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.all(AppPadding.p20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(ImageAssets.companyLogo),
-            SizedBox(height: mediaQuery.height * 0.10),
-            Text('Login',
-                style: getLightStyle(
-                    color: ColorManager.black, fontSize: FontSize.s40)),
-            SizedBox(height: mediaQuery.height * 0.05),
-            TextFormField(
-              controller: _emailController,
-              style: getRegularStyle(color: ColorManager.black),
-              decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email_outlined)),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: mediaQuery.height * 0.03),
-            TextFormField(
-              controller: _passwordController,
-              style: getRegularStyle(color: ColorManager.black),
-              decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.password_outlined)),
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-            ),
-            SizedBox(height: mediaQuery.height * 0.03),
-            ElevatedButton(
-              onPressed: () {
-                final email = _emailController.text;
-                final password = _passwordController.text;
-                context.read<LoginBloc>().add(
-                      LoginEventLogIn(
-                        email: email,
-                        password: password,
-                      ),
-                    );
-              },
-              child: Text(
-                'Login',
-                style: getRegularStyle(
-                    color: ColorManager.white, fontSize: FontSize.s18),
+    return Form(
+      key: formKey,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.all(AppPadding.p20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(ImageAssets.companyLogo),
+              SizedBox(height: mediaQuery.height * 0.10),
+              Text('Login',
+                  style: getLightStyle(
+                      color: ColorManager.black, fontSize: FontSize.s40)),
+
+              SizedBox(height: mediaQuery.height * 0.05),
+
+              // Email text fied
+
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: ((value) => !EmailValidator.validate(value!)
+                    ? 'Enter a valid email'
+                    : null),
+                controller: _emailController,
+                style: getRegularStyle(color: ColorManager.black),
+                decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email_outlined)),
+                keyboardType: TextInputType.emailAddress,
               ),
-            ),
-            SizedBox(height: mediaQuery.height * 0.12),
-            const Text('Do not have an account yet?'),
-            TextButton(
-              onPressed: () {
-                context.read<LoginBloc>().add(
-                      const LoginEventGoToRegistration(),
-                    );
-              },
-              child: const Text(
-                'Register here!',
+
+              SizedBox(height: mediaQuery.height * 0.03),
+
+              // Password text field
+
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) =>
+                    value!.length < 8 ? 'Password must be 8 characters' : null,
+                controller: _passwordController,
+                style: getRegularStyle(color: ColorManager.black),
+                decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.password_outlined)),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
               ),
-            )
-          ],
+              SizedBox(height: mediaQuery.height * 0.03),
+              ElevatedButton(
+                onPressed: () {
+                  final email = _emailController.text;
+                  final password = _passwordController.text;
+                  context.read<LoginBloc>().add(
+                        LoginEventLogIn(
+                          email: email,
+                          password: password,
+                        ),
+                      );
+                },
+                child: Text(
+                  'Login',
+                  style: getRegularStyle(
+                      color: ColorManager.white, fontSize: FontSize.s18),
+                ),
+              ),
+              SizedBox(height: mediaQuery.height * 0.12),
+              const Text('Do not have an account yet?'),
+              TextButton(
+                onPressed: () {
+                  context.read<LoginBloc>().add(
+                        const LoginEventGoToRegistration(),
+                      );
+                },
+                child: const Text(
+                  'Register here!',
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/login/register_screen.dart
+++ lib/presentation/login/register_screen.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:one_earth/presentation/resources/assets_manager.dart';
 import 'package:one_earth/presentation/resources/values_manager.dart';
-import 'package:one_earth/temp/if_debugging.dart';
 import 'package:one_earth/presentation/login/bloc/login_bloc.dart';
 import 'package:one_earth/presentation/login/bloc/login_event.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:string_validator/string_validator.dart';
 
 import '../resources/color_manager.dart';
 import '../resources/font_manager.dart';
@@ -24,6 +25,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _confirmPasswordController =
       TextEditingController();
 
+  final formKey = GlobalKey<FormState>();
+  bool isValid = false;
+
   @override
   void initState() {
     super.initState();
@@ -38,92 +42,147 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
+    _emailController.addListener(
+      () {
+        if (EmailValidator.validate(_emailController.text)) {
+          setState(() {
+            isValid = true;
+          });
+        } else {
+          setState(
+            () {
+              isValid = false;
+            },
+          );
+        }
+      },
+    );
+
     var mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.all(AppPadding.p20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(ImageAssets.companyLogo),
-            SizedBox(height: mediaQuery.height * 0.08),
-            Text('Register',
-                style: getLightStyle(
-                    color: ColorManager.black, fontSize: FontSize.s40)),
-            SizedBox(height: mediaQuery.height * 0.05),
-            TextFormField(
-              controller: _nameController,
-              style: getRegularStyle(color: ColorManager.black),
-              decoration: const InputDecoration(
-                  labelText: 'Name',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.person_outlined)),
-              keyboardType: TextInputType.emailAddress,
+      body: Form(
+        key: formKey,
+        // autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Padding(
+          padding: const EdgeInsets.all(AppPadding.p20),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(ImageAssets.companyLogo),
+                SizedBox(height: mediaQuery.height * 0.08),
+                Text('Register',
+                    style: getLightStyle(
+                        color: ColorManager.black, fontSize: FontSize.s40)),
+                SizedBox(height: mediaQuery.height * 0.05),
+
+                // Name text field
+
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _nameController,
+                  style: getRegularStyle(color: ColorManager.black),
+                  validator: (value) => value!.isEmpty || !isAlpha(value)
+                      ? 'Enter valid name'
+                      : null,
+                  decoration: const InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person_outlined)),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+
+                SizedBox(height: mediaQuery.height * 0.03),
+
+                // Email text field
+
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: ((value) => !EmailValidator.validate(value!)
+                      ? 'Enter a valid email'
+                      : null),
+                  controller: _emailController,
+                  style: getRegularStyle(color: ColorManager.black),
+                  decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email_outlined)),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+
+                SizedBox(height: mediaQuery.height * 0.03),
+
+                // Password text field
+
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _passwordController,
+                  style: getRegularStyle(color: ColorManager.black),
+                  validator: (value) => value!.length < 8
+                      ? 'Password must be 8 characters'
+                      : null,
+                  decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.password_outlined)),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                ),
+
+                SizedBox(height: mediaQuery.height * 0.03),
+
+                // Confirm password field
+
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _confirmPasswordController,
+                  style: getRegularStyle(color: ColorManager.black),
+                  validator: (value) => value! != _passwordController.text
+                      ? 'Your password does not match'
+                      : null,
+                  decoration: const InputDecoration(
+                      labelText: 'Confirm Password',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.password_outlined)),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                ),
+
+                SizedBox(height: mediaQuery.height * 0.03),
+
+                ElevatedButton(
+                  onPressed: () {
+                    final email = _emailController.text;
+                    final password = _passwordController.text;
+                    context.read<LoginBloc>().add(
+                          LoginEventRegister(
+                            email: email,
+                            password: password,
+                          ),
+                        );
+                  },
+                  child: Text(
+                    'Register',
+                    style: getRegularStyle(
+                        color: ColorManager.white, fontSize: FontSize.s18),
+                  ),
+                ),
+                SizedBox(height: mediaQuery.height * 0.07),
+                const Text('Already have an account?'),
+                TextButton(
+                  onPressed: () {
+                    context.read<LoginBloc>().add(
+                          const LoginEventGoToLogin(),
+                        );
+                  },
+                  child: const Text(
+                    'Login here!',
+                  ),
+                )
+              ],
             ),
-            SizedBox(height: mediaQuery.height * 0.03),
-            TextFormField(
-              controller: _emailController,
-              style: getRegularStyle(color: ColorManager.black),
-              decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email_outlined)),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: mediaQuery.height * 0.03),
-            TextFormField(
-              controller: _passwordController,
-              style: getRegularStyle(color: ColorManager.black),
-              decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.password_outlined)),
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-            ),
-            SizedBox(height: mediaQuery.height * 0.03),
-            TextFormField(
-              controller: _confirmPasswordController,
-              style: getRegularStyle(color: ColorManager.black),
-              decoration: const InputDecoration(
-                  labelText: 'Confirm Password',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.password_outlined)),
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-            ),
-            SizedBox(height: mediaQuery.height * 0.03),
-            ElevatedButton(
-              onPressed: () {
-                final email = _emailController.text;
-                final password = _passwordController.text;
-                context.read<LoginBloc>().add(
-                      LoginEventRegister(
-                        email: email,
-                        password: password,
-                      ),
-                    );
-              },
-              child: Text(
-                'Register',
-                style: getRegularStyle(
-                    color: ColorManager.white, fontSize: FontSize.s18),
-              ),
-            ),
-            SizedBox(height: mediaQuery.height * 0.07),
-            const Text('Already have an account?'),
-            TextButton(
-              onPressed: () {
-                context.read<LoginBloc>().add(
-                      const LoginEventGoToLogin(),
-                    );
-              },
-              child: const Text(
-                'Login here!',
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
