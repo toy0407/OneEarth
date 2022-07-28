4ccf23bfa13e8effc235e33461ea99168ddab94a
Subham Karmakar <50767716+SubhamKarmakar24@users.noreply.github.com>
2022-07-28 17:45:05 +0530

    News API text, errors not resolved

--- lib/presentation/main/pageviews/news/news_tabs/local_tab.dart
+++ lib/presentation/main/pageviews/news/news_tabs/local_tab.dart
@@ -1,39 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:one_earth/presentation/main/pageviews/news/model/article_model.dart';
 import 'package:one_earth/presentation/main/pageviews/news/news_tabs/newsListTile.dart';
+import 'package:one_earth/presentation/main/pageviews/news/services/api_services.dart';
 
 class LocalTabView extends StatelessWidget {
   final List data;
-  const LocalTabView({Key? key, required this.data}) : super(key: key);
+  LocalTabView({Key? key, required this.data}) : super(key: key);
+  
+  ApiService client = ApiService();
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        NewsListTile(
-            heading: 'heading',
-            description: 'description',
-            imageUrl: 'imageUrl'),
-        NewsListTile(
-            heading: 'heading',
-            description: 'description',
-            imageUrl: 'imageUrl'),
-        NewsListTile(
-            heading: 'heading',
-            description: 'description',
-            imageUrl: 'imageUrl'),
-        NewsListTile(
-            heading: 'heading',
-            description: 'description',
-            imageUrl: 'imageUrl'),
-        NewsListTile(
-            heading: 'heading',
-            description: 'description',
-            imageUrl: 'imageUrl'),
-        NewsListTile(
-            heading: 'heading',
-            description: 'description',
-            imageUrl: 'imageUrl'),
-      ],
-    );
+    return Scaffold(
+      body: FutureBuilder(
+        future: client.getArticle(),
+        builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+          if(snapshot.hasData){
+            List<Article>? articles = snapshot.data;
+
+            return ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (context, index) => 
+                ListTile(
+                  title: Text(articles[index].title.toString()),
+                  ),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+      );
+    // return ListView(
+    //   children: [
+    //     NewsListTile(
+    //         heading: 'heading',
+    //         description: FutureBuilder(
+    //           future: client.getArticle(),
+    //           builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+    //             return Center(
+    //               child: CircularProgressIndicator(),
+    //             )
+    //           },
+    //         ),
+    //         imageUrl: 'imageUrl'),
+    //     NewsListTile(
+    //         heading: 'heading',
+    //         description: 'description',
+    //         imageUrl: 'imageUrl'),
+    //     NewsListTile(
+    //         heading: 'heading',
+    //         description: 'description',
+    //         imageUrl: 'imageUrl'),
+    //     NewsListTile(
+    //         heading: 'heading',
+    //         description: 'description',
+    //         imageUrl: 'imageUrl'),
+    //     NewsListTile(
+    //         heading: 'heading',
+    //         description: 'description',
+    //         imageUrl: 'imageUrl'),
+    //     NewsListTile(
+    //         heading: 'heading',
+    //         description: 'description',
+    //         imageUrl: 'imageUrl'),
+    //   ],
+    // );
   }
 }
---
+++ lib/presentation/main/pageviews/news/services/api_services.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:one_earth/presentation/main/pageviews/news/model/article_model.dart';
+
+class ApiService {
+  
+  final endPointUrl =
+      "";
+
+  Future<List<Article>> getArticle() async {
+    Response res = await get(Uri.parse(endPointUrl));
+
+    if (res.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(res.body);
+
+      List<dynamic> body = json['articles'];
+
+      List<Article> articles =
+          body.map((dynamic item) => Article.fromJson(item)).toList();
+
+      return articles;
+    } else {
+      throw ("Can't get the Articles");
+    }
+  }
+}
