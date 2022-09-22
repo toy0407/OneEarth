885e5cfd90583b0aea9ccecb6ec0ca2ad514864b
toy0407 <bose.suvro@gmail.com>
2022-09-22 21:14:45 +0530

    Made Tutorial and Event Page UI

--- lib/presentation/main/pageviews/tutorial/tutorial_page.dart
+++ lib/presentation/main/pageviews/tutorial/tutorial_page.dart
@@ -1,7 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:one_earth/data/tutorial/tutorial_data.dart';
 import 'package:one_earth/presentation/resources/color_manager.dart';
+import 'package:one_earth/presentation/resources/font_manager.dart';
 import 'package:one_earth/presentation/resources/styles_manager.dart';
+import 'package:one_earth/presentation/resources/values_manager.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../resources/assets_manager.dart';
 
 class TutorialPage extends StatefulWidget {
   final List<TutorialData>? data;
@@ -14,17 +20,87 @@ class TutorialPage extends StatefulWidget {
 class _TutorialPageState extends State<TutorialPage> {
   @override
   void initState() {
-    print(widget.data![1].image);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Tutorial Page is coming soon !',
-        style: getMediumStyle(color: ColorManager.darkPrimary),
+    return ListView.builder(
+        itemCount: widget.data!.length,
+        itemBuilder: ((context, index) {
+          return tutorialPageListItem(
+              (widget.data![index].title),
+              (widget.data![index].description),
+              (widget.data![index].image),
+              (widget.data![index].url));
+        }));
+  }
+
+  Widget tutorialPageListItem(
+      String title, String description, String imageUrl, String url) {
+    return GestureDetector(
+      onTap: () => _launchUrl(Uri.parse(url)),
+      child: Card(
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            CachedNetworkImage(
+              imageUrl: imageUrl,
+              placeholder: ((context, url) {
+                return Image.asset(ImageAssets.noImageAvailable);
+              }),
+            ),
+            Container(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: AppSize.s8, right: AppSize.s8),
+                      child: Text(
+                        title,
+                        style: getRegularStyle(
+                            color: ColorManager.white, fontSize: FontSize.s25),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: AppSize.s8,
+                          right: AppSize.s8,
+                          bottom: AppSize.s8),
+                      child: Text(
+                        description,
+                        style: getLightStyle(color: ColorManager.white),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ]),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(6, 0, 0, 0),
+                    Color.fromARGB(60, 0, 0, 0),
+                    Color.fromARGB(130, 0, 0, 0),
+                    Color.fromARGB(220, 0, 0, 0),
+                    Color.fromARGB(255, 0, 0, 0)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _launchUrl(Uri _url) async {
+    if (!await launchUrl(_url)) {
+      throw 'Could not launch $_url';
+    }
   }
 }
--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -28,6 +28,8 @@ class AppSize {
   static const double s18 = 18.0;
   static const double s20 = 20.0;
   static const double s28 = 28.0;
+  static const double s32 = 32.0;
+  static const double s36 = 36.0;
   static const double s40 = 40.0;
   static const double s60 = 60.0;
   static const double s65 = 65.0;
